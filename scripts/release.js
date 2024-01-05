82af2fb4f1b701491cc1b59a835da967522dce1b
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-01-05 16:29:22 +0000

    chore(setup): release/publish script

--- scripts/release.js
+++ scripts/release.js
@@ -1,12 +1,99 @@
-import { git } from "./_init.js";
+import { git, lerna, enforceCleanWorkingDirectory } from "./_init.js";
+import { log, actor, primary, success, COMMIT_MSGS } from "./_utils.js";
 
 /**
  *
  */
-export async function release() {
-  // get current branch
-  const branch = await git.branch();
-  console.log("Branch: ", branch);
-}
+(async function () {
+  const MAIN_BRANCH = "main"; // TODO: main branch name is hardcoded here
 
-release();
+  /* 1. first some chores */
+  // 1a. this is a safety measure to ensure releases are only published from the main branch
+  await actor(
+    git.branch().then((b) => {
+      if (b.current !== MAIN_BRANCH)
+        throw new Error(
+          `You can only perform releases from ${primary(MAIN_BRANCH)} branch`
+        );
+    }),
+    `Check current branch is ${MAIN_BRANCH}`
+  );
+
+  // 1b. ensure the working directory is clean. this is a safety measure to ensure
+  // you don't accidentally commit untracked files or uncommitted changes
+  await actor(
+    enforceCleanWorkingDirectory(),
+    "Ensure there are no untracked files or uncommitted changes"
+  );
+
+  /* 2. retrieve all commit logs from the latest tagged release */
+  // 2a. get all tags
+  const tags = await actor(git.tags(), "Get all tags");
+
+  // 2b. get logs from most recent tag to HEAD
+  const logs = await actor(
+    git.log({
+      format: "format:%s",
+      from: tags.latest?.hash,
+      multiLine: false,
+    }),
+    "Retrieve logs from most recent tag to HEAD"
+  );
+
+  /* 2c. check if there is a release pending since the last tagged release */
+  // if there is no release pending, throw an error which ends the process
+  await actor(
+    new Promise((resolve, reject) => {
+      const pendingRelease = logs.all.some((log) =>
+        log.message.includes(COMMIT_MSGS.RELEASE)
+      );
+      if (pendingRelease) {
+        resolve();
+      } else {
+        reject(
+          new Error(
+            "There is no release prepared since the last tagged release"
+          )
+        );
+      }
+    }),
+    "Release pending since last tagged release"
+  );
+
+  /* 3. publish release */
+  // 3a. get changed packages since last tagged release
+  const changedPackages = await actor(
+    lerna
+      .changed(undefined, { "--json": true }, { stdio: "pipe" })
+      .then(({ stdout }) => JSON.parse(stdout)),
+    "Changed packages since last tagged release"
+  );
+
+  // 3b. create annotated tags for each changed package
+  await Promise.all(
+    changedPackages.map(({ name, version }) => {
+      const tagName = `${name}@v${version}`;
+
+      return actor(
+        git.addAnnotatedTag(tagName, `${COMMIT_MSGS.TAG} ${tagName}`),
+        `Create ${tagName} annotated tag for ${name} package`
+      );
+    })
+  );
+
+  // 3c. publish changed packages to npm and create release on github
+  await actor(
+    lerna.publish(["from-package"], {
+      "--amend": true,
+      "--create-release": "github",
+    }),
+    "Publish packages to npm and create release on github"
+  );
+
+  /* 4. hey! we are done now 🚀 */
+  log(`\nProjects released successfully 🥁🥁🥁🥁: Here is the list\n`);
+  changedPackages.forEach(({ name, version }, index) =>
+    log(`${index + 1}.\t ${primary(name)} - v${success.bold(version)}\n`)
+  );
+  log(success.bold("\n\nEnd\n\n"));
+})();
