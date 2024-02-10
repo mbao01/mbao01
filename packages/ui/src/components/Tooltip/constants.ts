9812d407fdde1668f8de9ce215c81eb33a711794
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-02-10 18:54:28 +0000

    chore(import): clean up unused import

--- packages/ui/src/components/Tooltip/constants.ts
+++ packages/ui/src/components/Tooltip/constants.ts
@@ -1,26 +1,25 @@
-import { Tooltip } from './Tooltip';
-import c from 'clsx';
-import type { TooltipPosition, TooltipVariant } from './types';
+import c from "clsx";
+import type { TooltipPosition, TooltipVariant } from "./types";
 
 const TOOLTIP_POSITION = {
-  top: c('tooltip-top'),
-  right: c('tooltip-right'),
-  bottom: c('tooltip-bottom'),
-  left: c('tooltip-left'),
+  top: c("tooltip-top"),
+  right: c("tooltip-right"),
+  bottom: c("tooltip-bottom"),
+  left: c("tooltip-left"),
 } satisfies Record<TooltipPosition, string>;
 
 const TOOLTIP_VARIANTS = {
-  accent: c('tooltip-accent'),
-  default: c('tooltip-default'),
-  error: c('tooltip-error'),
-  ghost: c('tooltip-ghost'),
-  info: c('tooltip-info'),
-  link: c('tooltip-link'),
-  neutral: c('tooltip-neutral'),
-  primary: c('tooltip-primary'),
-  secondary: c('tooltip-secondary'),
-  success: c('tooltip-success'),
-  warning: c('tooltip-warning'),
+  accent: c("tooltip-accent"),
+  default: c("tooltip-default"),
+  error: c("tooltip-error"),
+  ghost: c("tooltip-ghost"),
+  info: c("tooltip-info"),
+  link: c("tooltip-link"),
+  neutral: c("tooltip-neutral"),
+  primary: c("tooltip-primary"),
+  secondary: c("tooltip-secondary"),
+  success: c("tooltip-success"),
+  warning: c("tooltip-warning"),
 } satisfies Record<TooltipVariant, string>;
 
 export const getTooltipClasses = ({
@@ -30,5 +29,5 @@ export const getTooltipClasses = ({
   variant?: TooltipVariant;
   position?: TooltipPosition;
 }) => {
-  return c('tooltip', TOOLTIP_VARIANTS[variant!], TOOLTIP_POSITION[position!]);
+  return c("tooltip", TOOLTIP_VARIANTS[variant!], TOOLTIP_POSITION[position!]);
 };
