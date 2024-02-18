c893cff3c1f2ef544a2880304dea00fb775672bf
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-02-18 20:58:55 +0000

    chore(common): test for card and tooltip components

--- packages/common/src/components/Tooltip/Tooltip.test.tsx
+++ packages/common/src/components/Tooltip/Tooltip.test.tsx
@@ -8,12 +8,14 @@ describe("Tooltip", () => {
     variant,
     children,
     position,
-  }: TooltipProps) =>
-    render(
+  }: TooltipProps) => {
+    return render(
       <Tooltip content={content} variant={variant} position={position}>
         {children}
       </Tooltip>
     );
+  };
+
   it("shows tooltip with content", () => {
     const { asFragment } = renderTooltip({
       content: "I am the tooltip's content",
@@ -25,6 +27,49 @@ describe("Tooltip", () => {
     expect(tooltipEl).toBeVisible();
     expect(tooltipEl).toHaveClass("tooltip");
     expect(tooltipEl).toHaveAttribute("data-tip", "I am the tooltip's content");
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it.each(["top", "right", "bottom", "left"] as const)(
+    "content is positioned %s",
+    (position) => {
+      const content = `tooltip content ${position}`;
+      const trigger = `${position} tooltip`;
+      const { asFragment } = render(
+        <Tooltip content={content} position={position}>
+          {trigger}
+        </Tooltip>
+      );
+
+      const tooltipEl = screen.getByText(trigger);
+      expect(tooltipEl).toHaveAttribute("data-tip", content);
+      expect(asFragment()).toMatchSnapshot();
+    }
+  );
+
+  it.each([
+    "accent",
+    "default",
+    "error",
+    "ghost",
+    "info",
+    "link",
+    "neutral",
+    "primary",
+    "secondary",
+    "success",
+    "warning",
+  ] as const)("has %s variant", (variant) => {
+    const content = `${variant} tooltip content`;
+    const trigger = `${variant} tooltip`;
+    const { asFragment } = render(
+      <Tooltip content={content} variant={variant}>
+        {trigger}
+      </Tooltip>
+    );
+
+    const tooltipEl = screen.getByText(trigger);
+    expect(tooltipEl).toHaveAttribute("data-tip", content);
     expect(asFragment()).toMatchSnapshot();
   });
 });
