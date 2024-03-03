c96a598fc1c1ea4983db46d3299c7a687ca05660
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-03-03 15:12:20 +0000

    🔧 chore: update link component to use cva, fix butg with target prop

--- packages/ui/src/components/Link/Link.tsx
+++ packages/ui/src/components/Link/Link.tsx
@@ -1,8 +1,8 @@
-import c from "clsx";
 import { Link as RouterLink } from "react-router-dom";
-import { getLinkClasses } from "./constant";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { cn } from "@mbao01/common/helpers";
 import { type LinkProps } from "./types";
-import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
+import { getLinkClasses } from "./constant";
 
 export const Link = ({
   href,
@@ -15,16 +15,14 @@ export const Link = ({
 }: LinkProps) => {
   return (
     <RouterLink
-      {...props}
       to={href}
-      className={c(getLinkClasses({ hover, variant }), className)}
+      target={target}
+      className={cn(getLinkClasses({ hover, variant }), className)}
+      {...props}
     >
       {children}
       {target === "_blank" && (
-        <ArrowUpRightIcon
-          name="external"
-          className="ml-[2px] inline h-4 w-4 align-middle"
-        />
+        <ExternalLinkIcon name="external" className="ml-[2px] inline" />
       )}
     </RouterLink>
   );
