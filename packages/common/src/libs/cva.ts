a9453f5481300b2be6ef6f5530a080625a170120
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-04-13 00:29:41 +0100

    🔧 fix: update VariantProps to exclude null values and configure react-docgen-typescript options in Storybook config

--- packages/common/src/libs/cva.ts
+++ packages/common/src/libs/cva.ts
@@ -1,1 +1,9 @@
-export { cva, type VariantProps } from "class-variance-authority";
+import { type VariantProps as OriginalVariantProps } from "class-variance-authority";
+
+export { cva } from "class-variance-authority";
+
+type ExcludeNull<T> = { [P in keyof T]: Exclude<T[P], null> };
+
+export type VariantProps<T extends (...args: any) => any> = ExcludeNull<
+  OriginalVariantProps<T>
+>;
