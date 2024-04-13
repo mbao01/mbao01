fedbae1214780eff41d9db379b1aef387b8e1b9d
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-04-13 01:28:14 +0100

    🔧 fix: adjust styling classes for consistency in input fields.

--- packages/common/src/libs/cva.ts
+++ packages/common/src/libs/cva.ts
@@ -4,6 +4,7 @@ export { cva } from "class-variance-authority";
 
 type ExcludeNull<T> = { [P in keyof T]: Exclude<T[P], null> };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type VariantProps<T extends (...args: any) => any> = ExcludeNull<
   OriginalVariantProps<T>
 >;
