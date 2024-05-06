85d87ddfd16cad9ac1272b41546742523e1236ca
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-05-06 23:51:41 +0100

    feat(label): use text component as label text

--- packages/common/src/components/Form/components/Label/types.ts
+++ packages/common/src/components/Form/components/Label/types.ts
@@ -1,10 +1,11 @@
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { type VariantProps } from "../../../../libs";
 import { getLabelClasses } from "./constants";
+import { Text } from "../../../Text";
 
 export type LabelProps = React.ComponentPropsWithoutRef<
   typeof LabelPrimitive.Root
 > &
   VariantProps<typeof getLabelClasses>;
 
-export type LabelTextProps = React.HTMLAttributes<HTMLSpanElement>;
+export type LabelTextProps = Omit<React.ComponentProps<typeof Text>, "as">;
