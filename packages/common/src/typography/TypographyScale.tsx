749f814afeb6a3c6452fedaccd3c2a907ce92e2e
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-08-19 14:57:31 +0100

    feat(typography): add demo and scale stories

---
+++ packages/common/src/typography/TypographyScale.tsx
@@ -0,0 +1,34 @@
+export const TypographyScale = () => {
+  return (
+    <div className="prose">
+      <h1>h1. Heading</h1>
+      <h2>h2. Heading</h2>
+      <h3>h3. Heading</h3>
+      <h4>h4. Heading</h4>
+
+      <sup>superscript text</sup>
+      <span>Normal text here</span>
+      <sub>subscript text</sub>
+
+      <p>
+        Paragraph. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </p>
+
+      <button type="button">Primary button text</button>
+      <blockquote>
+        Blockquote: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur unde suscipit, quam beatae
+      </blockquote>
+      <dl>
+        <dt>Description term</dt>
+        <dd>
+          Description detail: Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+        </dd>
+      </dl>
+    </div>
+  );
+};
