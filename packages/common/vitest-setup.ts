4756a8c5783797ca6abf0f9f1c1642e1b09b18e9
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-09-28 07:30:41 +0100

    feat(tests): mock matchMedia on window object

--- packages/common/vitest-setup.ts
+++ packages/common/vitest-setup.ts
@@ -15,6 +15,22 @@ vi.stubGlobal("ResizeObserver", ResizeObserverMock);
 // Mock the scrollTo method
 vi.stubGlobal("scrollTo", vi.fn());
 
+window.HTMLElement.prototype.scrollIntoView = () => {};
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
