454807fd0ac29edad258eb366d0d5d01cf6f629c
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-09-28 01:35:43 +0100

    feat: chore

--- packages/common/src/components/Collapsible/Collapsible.tsx
+++ packages/common/src/components/Collapsible/Collapsible.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
-import { type CollapsibleProps } from "@radix-ui/react-accordion";
+import { type CollapsibleProps } from "@radix-ui/react-collapsible";
 
 const Collapsible = (props: CollapsibleProps) => (
   <CollapsiblePrimitive.Root {...props} />
