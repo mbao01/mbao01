646b85aa741837f6ed5ef3136b713ff394be6bd4
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-10-11 22:10:55 +0100

    feat(piechart): add stories for pie chart

---
+++ packages/common/src/components/Chart/stories/PieChart.stories.tsx
@@ -0,0 +1,162 @@
+import type { Meta, StoryContext, StoryFn, StoryObj } from "@storybook/react";
+import type { PieChartArgs } from "./args";
+import { pieChartArgs } from "./args";
+import {
+  ActiveDonutPieChartExample,
+  CustomLabeledPieChartExample,
+  DonutPieChartExample,
+  LabeledPieChartExample,
+  LabelListPieChartExample,
+  PieChartExample,
+  StackedPieChartExample,
+  WithLegendPieChartExample,
+} from "./examples/PieChart";
+import { getArgsFromArgTypes, renderer, withArgs } from "./helpers";
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
+const withTheme = (Component: StoryFn, context: StoryContext<PieChartArgs>) => {
+  return (
+    <div>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        :root {
+          --chart-1: 221.2 83.2% 53.3%;
+          --chart-2: 212 95% 68%;
+          --chart-3: 216 92% 60%;
+          --chart-4: 210 98% 78%;
+          --chart-5: 212 97% 87%;
+        }
+        .dark {
+          --chart-1: 221.2 83.2% 53.3%;
+          --chart-2: 212 95% 68%;
+          --chart-3: 216 92% 60%;
+          --chart-4: 210 98% 78%;
+          --chart-5: 212 97% 87%;
+        }
+        `,
+        }}
+      />
+      <Component {...context.args} />
+    </div>
+  );
+};
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Components/Chart/Pie",
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+    // backgrounds: [{ name: 'dark background', value: '#000', default: true }],
+    a11y: {
+      config: {
+        rules: [
+          {
+            // @fixme
+            id: "color-contrast",
+            reviewOnFail: true,
+          },
+        ],
+      },
+    },
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: pieChartArgs,
+  args: getArgsFromArgTypes(pieChartArgs),
+  decorators: [withTheme, withArgs],
+} satisfies Meta<PieChartArgs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie nameKey": "browser",
+  },
+  render: renderer(PieChartExample),
+};
+
+export const PieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie nameKey": "browser",
+  },
+  render: renderer(PieChartExample),
+};
+
+export const LabeledPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie label": true,
+    "pie nameKey": "browser",
+  },
+  render: renderer(LabeledPieChartExample),
+};
+
+export const CustomLabeledPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie labelLine": false,
+    "pie nameKey": "browser",
+  },
+  render: renderer(CustomLabeledPieChartExample),
+};
+
+export const LabelListPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+  },
+  render: renderer(LabelListPieChartExample),
+};
+
+export const WithLegendPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+  },
+  render: renderer(WithLegendPieChartExample),
+};
+
+export const DonutPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie nameKey": "browser",
+    "pie innerRadius": 60,
+  },
+  render: renderer(DonutPieChartExample),
+};
+
+export const ActiveDonutPieChart: Story = {
+  args: {
+    "pie data": chartData,
+    "pie dataKey": "visitors",
+    "pie nameKey": "browser",
+    "pie innerRadius": 60,
+    "pie strokeWidth": 5,
+    "pie activeIndex": 0,
+  },
+  render: renderer(ActiveDonutPieChartExample),
+};
+
+export const StackedPieChart: Story = {
+  args: {},
+  render: renderer(StackedPieChartExample),
+};
---
+++ packages/common/src/components/Chart/stories/examples/PieChart.tsx
@@ -0,0 +1,234 @@
+import { LabelList, Pie, PieChart, Sector } from "recharts";
+import { PieSectorDataItem } from "recharts/types/polar/Pie";
+import { Chart } from "../../Chart";
+import {
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "../../components";
+import { type ChartConfig } from "../../types";
+import { PieChartProps } from "../args";
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
+
+/**
+ * ## Parent
+ *
+ * The PieChart can be used within: `<Chart />`
+ *
+ * ## Children
+ *
+ * The PieChart can be used with the following child components: `<PolarAngleAxis />`, `<PolarRadiusAxis />`,
+ * `<PolarGrid />`, `<Legend />`, `<Tooltip />`, `<Pie />`, `<Customized />`
+ */
+export const PieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="h-[250px] w-full">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+        <Pie dataKey="visitors" {...props.pie} />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const LabeledPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart
+      config={chartConfig}
+      className="mx-auto aspect-square h-[250px] pb-0 [&_.recharts-pie-label-text]:fill-base-content"
+    >
+      <PieChart {...props.pieChart}>
+        <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+        <Pie dataKey="visitors" {...props.pie} />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const CustomLabeledPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip content={<ChartTooltipContent nameKey="visitors" hideLabel />} />
+        <Pie
+          dataKey="visitors"
+          {...props.pie}
+          label={({ payload, ...props }) => {
+            return (
+              <text
+                cx={props.cx}
+                cy={props.cy}
+                x={props.x}
+                y={props.y}
+                textAnchor={props.textAnchor}
+                dominantBaseline={props.dominantBaseline}
+                className="fill-base-content"
+              >
+                {`${
+                  chartConfig[payload.browser as keyof typeof chartConfig]?.label
+                } (${payload.visitors})`}
+              </text>
+            );
+          }}
+        />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const LabelListPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip content={<ChartTooltipContent nameKey="visitors" hideLabel />} />
+        <Pie dataKey="visitors" {...props.pie}>
+          <LabelList
+            dataKey="browser"
+            className="fill-base-100"
+            stroke="none"
+            fontSize={12}
+            formatter={(value: keyof typeof chartConfig) => chartConfig[value]?.label}
+            {...props}
+          />
+        </Pie>
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const WithLegendPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <Pie dataKey="visitors" {...props.pie} />
+        <ChartLegend
+          content={<ChartLegendContent nameKey="browser" />}
+          className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+        />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const DonutPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+        <Pie dataKey="visitors" {...props.pie} />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const ActiveDonutPieChartExample = (props: PieChartProps) => {
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+        <Pie
+          dataKey="visitors"
+          {...props.pie}
+          activeShape={({ outerRadius = 0, ...props }: PieSectorDataItem) => (
+            <Sector {...props} outerRadius={outerRadius + 10} />
+          )}
+        />
+      </PieChart>
+    </Chart>
+  );
+};
+
+export const StackedPieChartExample = (props: PieChartProps) => {
+  const desktopData = [
+    { month: "january", desktop: 186, fill: "var(--color-january)" },
+    { month: "february", desktop: 305, fill: "var(--color-february)" },
+    { month: "march", desktop: 237, fill: "var(--color-march)" },
+    { month: "april", desktop: 173, fill: "var(--color-april)" },
+    { month: "may", desktop: 209, fill: "var(--color-may)" },
+  ];
+  const mobileData = [
+    { month: "january", mobile: 80, fill: "var(--color-january)" },
+    { month: "february", mobile: 200, fill: "var(--color-february)" },
+    { month: "march", mobile: 120, fill: "var(--color-march)" },
+    { month: "april", mobile: 190, fill: "var(--color-april)" },
+    { month: "may", mobile: 130, fill: "var(--color-may)" },
+  ];
+  const chartConfig = {
+    visitors: {
+      label: "Visitors",
+    },
+    desktop: {
+      label: "Desktop",
+    },
+    mobile: {
+      label: "Mobile",
+    },
+    january: {
+      label: "January",
+      color: "hsl(var(--chart-1))",
+    },
+    february: {
+      label: "February",
+      color: "hsl(var(--chart-2))",
+    },
+    march: {
+      label: "March",
+      color: "hsl(var(--chart-3))",
+    },
+    april: {
+      label: "April",
+      color: "hsl(var(--chart-4))",
+    },
+    may: {
+      label: "May",
+      color: "hsl(var(--chart-5))",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <Chart config={chartConfig} className="mx-auto aspect-square h-[250px]">
+      <PieChart {...props.pieChart}>
+        <ChartTooltip
+          content={
+            <ChartTooltipContent
+              labelKey="visitors"
+              nameKey="month"
+              indicator="line"
+              labelFormatter={(_, payload) => {
+                return chartConfig[payload?.[0].dataKey as keyof typeof chartConfig].label;
+              }}
+            />
+          }
+        />
+        <Pie {...props.pie} data={desktopData} dataKey="desktop" outerRadius={60} />
+        <Pie {...props.pie} data={mobileData} dataKey="mobile" innerRadius={70} outerRadius={90} />
+      </PieChart>
+    </Chart>
+  );
+};
