486a0a12273e0564a009ff90833c6a5fb14ec49b
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-10-11 09:18:44 +0100

    feat(linechart): add stories for line chart

--- packages/common/src/components/Chart/stories/BarChart.stories.tsx
+++ packages/common/src/components/Chart/stories/BarChart.stories.tsx
@@ -78,7 +78,7 @@ const meta = {
 } satisfies Meta<BarChartArgs>;
 
 export default meta;
-type Story = StoryObj<BarChartArgs>;
+type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
--- packages/common/src/components/Chart/stories/args/index.ts
+++ packages/common/src/components/Chart/stories/args/index.ts
@@ -1,8 +1,10 @@
 import { PropsWithoutRef } from "react";
 import { type ArgTypes } from "@storybook/react";
-import { BarProps, XAxisProps, YAxisProps } from "recharts";
+import { BarProps, LineProps, XAxisProps, YAxisProps } from "recharts";
 import { CategoricalChartProps } from "recharts/types/chart/generateCategoricalChart";
 import { barChartArgs as barChartComponentArgs } from "../args/barChartArgs";
+import { lineChartArgs as lineChartComponentArgs } from "../args/lineChartArgs";
+import { lineStyleArgs } from "../args/stylesArgs";
 import { categorizeArgs } from "../helpers";
 import { barArgs } from "./barArgs";
 import { Flatten } from "./types";
@@ -32,4 +34,29 @@ export const barChartArgs = {
   ...categorizeArgs(xAxisArgs, barArgKey.xAxis),
   ...categorizeArgs(yAxisArgs, barArgKey.yAxis),
   ...categorizeArgs(barArgs, barArgKey.bar),
+} satisfies ArgTypes;
+
+type LineArgKey = "line" | "lineChart" | "xAxis" | "yAxis";
+
+export type LineChartProps = Partial<{
+  line: PropsWithoutRef<LineProps>;
+  lineChart: CategoricalChartProps;
+  xAxis: XAxisProps;
+  yAxis: YAxisProps;
+}>;
+
+export type LineChartArgs = Partial<NonNullable<Flatten<Required<LineChartProps>>>>;
+
+export const argsKeys = {
+  line: "line",
+  lineChart: "lineChart",
+  xAxis: "xAxis",
+  yAxis: "yAxis",
+} satisfies Record<string, LineArgKey>;
+
+export const lineChartArgs = {
+  ...categorizeArgs(lineChartComponentArgs, argsKeys.lineChart),
+  ...categorizeArgs(xAxisArgs, argsKeys.xAxis),
+  ...categorizeArgs(yAxisArgs, argsKeys.yAxis),
+  ...categorizeArgs(lineStyleArgs, argsKeys.line),
 } satisfies ArgTypes;
