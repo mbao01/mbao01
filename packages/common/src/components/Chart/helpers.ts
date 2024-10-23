a7982a6cac435c2b571da9a38186fed28d5bb23c
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-10-23 22:40:28 +0100

    feat(types): update types

--- packages/common/src/components/Chart/helpers.ts
+++ packages/common/src/components/Chart/helpers.ts
@@ -1,11 +1,15 @@
+import type {
+  NameType,
+  Payload as TooltipPayload,
+  ValueType,
+} from "recharts/types/component/DefaultTooltipContent";
 import { type Payload as LegendPayload } from "recharts/types/component/DefaultLegendContent";
-import { type Payload as TooltipPayload } from "recharts/types/component/DefaultTooltipContent";
 import { type ChartConfig } from "./types";
 
 // Helper to extract item config from a payload.
 export const getPayloadConfigFromPayload = (
   config: ChartConfig,
-  payload: LegendPayload | TooltipPayload<any, any>,
+  payload: LegendPayload | TooltipPayload<ValueType, NameType>,
   key: string
 ) => {
   if (typeof payload !== "object" || payload === null) {
@@ -14,19 +18,15 @@ export const getPayloadConfigFromPayload = (
 
   const payloadPayload =
     "payload" in payload && typeof payload.payload === "object" && payload.payload !== null
-      ? payload.payload
+      ? (payload.payload as Record<string, unknown>)
       : undefined;
 
   let configLabelKey: string = key;
 
   if (key in payload && typeof payload[key as keyof typeof payload] === "string") {
     configLabelKey = payload[key as keyof typeof payload] as string;
-  } else if (
-    payloadPayload &&
-    key in payloadPayload &&
-    typeof payloadPayload[key as keyof typeof payloadPayload] === "string"
-  ) {
-    configLabelKey = payloadPayload[key as keyof typeof payloadPayload] as string;
+  } else if (payloadPayload && key in payloadPayload && typeof payloadPayload[key] === "string") {
+    configLabelKey = payloadPayload[key];
   }
 
   return configLabelKey in config ? config[configLabelKey] : config[key];
