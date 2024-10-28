8f422bc5defefed28b568579fc8fc8a98ce811bc
Ayomide Bakare <bakkareayomideo@gmail.com>
2024-10-28 00:37:43 +0000

    test(sidebar-stories): add stories for sidebar

---
+++ packages/common/src/components/Sidebar/stories/Sidebar.stories.tsx
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  SidebarExample,
+  SidebarInADialogExample,
+  SidebarLeftAndRightExample,
+  SidebarOnTheRightExample,
+  SidebarWithCollapsibleGroupExample,
+  SidebarWithCollapsibleTreeExample,
+  SidebarWithSecondaryNavigationExample,
+  SidebarWithSubMenuExample,
+} from "./examples/Sidebar.example";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Components/Sidebar",
+  component: SidebarExample,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof SidebarExample>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {},
+};
+
+export const SidebarWithCollapsibleGroup: Story = {
+  args: {},
+  render: (props) => <SidebarWithCollapsibleGroupExample {...props} />,
+};
+
+export const SidebarWithSubMenu: Story = {
+  args: {},
+  render: (props) => <SidebarWithSubMenuExample {...props} />,
+};
+
+export const SidebarWithIcons: Story = {
+  args: {
+    collapsible: "icon",
+  },
+  render: (props) => <SidebarWithSubMenuExample {...props} />,
+};
+
+export const SidebarWithSecondaryNavigation: Story = {
+  args: {
+    variant: "inset",
+  },
+  render: (props) => <SidebarWithSecondaryNavigationExample {...props} />,
+};
+
+export const SidebarWithCollapsibleTree: Story = {
+  args: {
+    collapsible: "none",
+  },
+  render: (props) => <SidebarWithCollapsibleTreeExample {...props} />,
+};
+
+export const SidebarInADialog: Story = {
+  args: {},
+  render: (props) => <SidebarInADialogExample {...props} />,
+};
+
+export const SidebarOnTheRight: Story = {
+  args: {
+    side: "right",
+  },
+  render: (props) => <SidebarOnTheRightExample {...props} />,
+};
+
+export const SidebarLeftAndRight: Story = {
+  args: {},
+  render: (props) => <SidebarLeftAndRightExample {...props} />,
+};
