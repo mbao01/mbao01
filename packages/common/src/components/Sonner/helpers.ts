f2441e798329815b56d6f2dee62db91b51582c3f
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-03-12 08:31:59 +0000

    feat(sonner): export sonner classes

---
+++ packages/common/src/components/Sonner/helpers.ts
@@ -0,0 +1,24 @@
+import type { ToasterProps } from "./types";
+import { cn } from "../../utilities";
+import { getToastButtonClasses, getToastClasses } from "./constants";
+
+export const toastClassnames = ({
+  variant,
+  outline,
+  actionButton,
+  cancelButton,
+  closeButton,
+}: Pick<ToasterProps, "variant" | "outline" | "actionButton" | "cancelButton" | "closeButton">) => {
+  return {
+    toast: cn(getToastClasses({ variant, outline })),
+    actionButton: cn(getToastButtonClasses(actionButton)),
+    cancelButton: cn(
+      getToastButtonClasses({
+        outline: true,
+        variant: "base",
+        ...cancelButton,
+      })
+    ),
+    closeButton: cn(getToastButtonClasses({ variant: "neutral", ...closeButton })),
+  };
+};
