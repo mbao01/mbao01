3cb7009f5b4102a0939ac4f851e828ec58b6d99d
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-03-31 14:24:36 +0100

    feat(deps): update tailwind in next package

--- packages/next/tailwind.config.ts
+++ packages/next/tailwind.config.ts
@@ -2,6 +2,6 @@ import { type Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "node_modules/@mbao01/common/src/**/*"],
-  plugins: [...require("@mbao01/common/plugin")],
+  // plugins: [...require("@mbao01/common/plugin")],
 };
 export default config;
