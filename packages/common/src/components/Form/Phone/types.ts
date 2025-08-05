24543a2862b1ecab763406eec3bcf55ef4ec3519
Ayomide Bakare <mbao01@users.noreply.github.com>
2025-08-05 21:39:49 +0100

    Allow default country code (#198)
    * feat(phone): allow default country for phone input
    
    * test(phone): update both unit and visual tests for phone component

--- packages/common/src/components/Form/Phone/types.ts
+++ packages/common/src/components/Form/Phone/types.ts
@@ -1,3 +1,5 @@
+import { UsePhoneInputConfig } from "react-international-phone";
 import { type InputProps } from "../Input/types";
 
-export type PhoneProps = Omit<InputProps, "type" | "label" | "inputMode">;
+export type PhoneProps = Omit<InputProps, "type" | "label" | "inputMode"> &
+  Pick<UsePhoneInputConfig, "defaultCountry">;
