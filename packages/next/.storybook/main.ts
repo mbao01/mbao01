be3c876c3d18506d4ca1f831ea06b1ef5b944650
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-12-10 14:42:41 +0000

    chore(storybook): remove duplicate addon in storybook/main

--- packages/next/.storybook/main.ts
+++ packages/next/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
     "@storybook/addon-a11y",
@@ -9,7 +9,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-onboarding",
     "@storybook/addon-themes",
-    "@storybook/addon-docs",
     "@storybook/addon-docs",
   ],
 
