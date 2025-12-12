50cfaa7fb386117b637606e5fa0104ce4d4a673c
Ayomide Bakare <mbao01@users.noreply.github.com>
2025-12-12 01:47:04 +0000

    Add separator variants and sizes (#206)
    * feat(separator): add separator variant and size
    
    * test(visual): add screenshot for separator stories
    
    * test(unit): update separator snapshots

--- packages/common/src/components/Separator/types.ts
+++ packages/common/src/components/Separator/types.ts
@@ -1,3 +1,6 @@
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import { type VariantProps } from "../../libs";
+import { getSeparatorClasses } from "./constants";
 
-export type SeparatorProps = React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>;
+export type SeparatorProps = React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root> &
+  VariantProps<typeof getSeparatorClasses>;
