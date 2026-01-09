44347197a3d6451bd7bd1a9b10774528bb1ae528
Ayomide Bakare <mbao01@users.noreply.github.com>
2026-01-09 20:42:36 +0000

    chore(release): trigger release (#215)

--- packages/common/src/utilities/theme.ts
+++ packages/common/src/utilities/theme.ts
@@ -5,14 +5,18 @@ export type Theme = "dark" | "light";
 export const THEME_COOKIE_NAME = "data-theme";
 
 const getSystemTheme = () => {
-  if (typeof window === "undefined") return null;
+  if (typeof window === "undefined") {
+    return null;
+  }
 
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   return prefersDark ? "dark" : "light";
 };
 
 export const getTheme = () => {
-  if (typeof window === "undefined") return null;
+  if (typeof window === "undefined") {
+    return null;
+  }
 
   const cookies = new Cookies();
   const theme = (cookies.get(THEME_COOKIE_NAME) ?? getSystemTheme()) as Theme | null;
